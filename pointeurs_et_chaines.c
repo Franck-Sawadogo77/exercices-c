f5a59e7269f7c64d1329b540871055b6882b6171
Franck-Sawadogo <tradernewabc@gmail.com>
2026-03-31 12:48:01 +0000

    Add files via upload

---
+++ pointeurs_et_chaines.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void) {
+    char mot[100];
+    char* p = mot;
+    int i =0;
+    int nbre = 0;
+    
+    printf("Entrez le mot: ");
+    scanf("%s",mot);
+    
+    while(*(p+i) != '\0') {
+    	nbre += 1;
+    	i++;
+	}
+	
+	printf("Nombre de caractères: %d",nbre);
+    return 0;
+}
